4f80cb881a38c74f9b6eba0c7a95a5e583c509c4
M_reza <m.reza79me@gmail.com>
2025-08-27 06:37:02 +0330

    add viaw country

--- lib/views/country_detail_screen.dart
+++ lib/views/country_detail_screen.dart
@@ -228,8 +228,11 @@ class _CountryDetailScreenState
                                         textColor,
                                   ),
                                 ),
+                                SizedBox(
+                                  height: 5,
+                                ),
                                 Text(
-                                  'Native Name:${countryDetails[0].nativeName} ',
+                                  'Native Name: ${countryDetails[0].nativeName} ',
                                   style: TextStyle(
                                     fontSize: 20,
                                     color:
@@ -237,9 +240,85 @@ class _CountryDetailScreenState
                                   ),
                                 ),
                                 SizedBox(
-                                  height: 10,
+                                  height: 5,
                                 ),
-                                Divider(),
+                                Text(
+                                  'Population: ${countryDetails[0].population} ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color:
+                                        textColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  'Region: ${countryDetails[0].region} ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color:
+                                        textColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  'Sub Region: ${countryDetails[0].subregion} ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color:
+                                        textColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  'capita: ${countryDetails[0].capital.join()} ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color:
+                                        textColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Text(
+                                  'Top Level Domain: ${countryDetails[0].tld[0]} ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color:
+                                        textColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  'Continents: ${countryDetails[0].continents.join()} ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color:
+                                        textColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  'Languages: ${countryDetails[0].languages.join(', ')} ',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color:
+                                        textColor,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
                               ],
                             ),
                           ),
@@ -248,67 +327,78 @@ class _CountryDetailScreenState
                     ),
                   ],
 
-                  SliverToBoxAdapter(
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.only(
-                            right: 20,
-                            left: 20,
-                            bottom: 10,
-                          ),
-                      child: Column(
-                        mainAxisAlignment:
-                            MainAxisAlignment
-                                .center,
-                        crossAxisAlignment:
-                            CrossAxisAlignment
-                                .start,
-                        children: [
-                          Text(
-                            'Description: ${widget.country.population}',
-                            style: TextStyle(
-                              fontSize: 24,
+                  if (state
+                      is CountryDetailLoadedState) ...[
+                    state.borderCountries.fold(
+                      (l) {
+                        return SliverToBoxAdapter(
+                          child: Text(l),
+                        );
+                      },
+                      (countryDetails) {
+                        return SliverToBoxAdapter(
+                          child: Container(
+                            margin:
+                                EdgeInsets.only(
+                                  left: 10,
+                                ),
+                            height:
+                                50, // ارتفاع لیست افقی
+                            child: ListView.builder(
+                              scrollDirection:
+                                  Axis.horizontal,
+                              itemCount:
+                                  countryDetails[0]
+                                      .borders
+                                      .length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(
+                                        horizontal:
+                                            4.0,
+                                        vertical:
+                                            5,
+                                      ),
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      iconColor:
+                                          textColor,
+                                      iconSize:
+                                          20,
 
-                              color: textColor,
+                                      elevation:
+                                          3,
+                                      shadowColor:
+                                          Colors
+                                              .black,
+                                    ),
+                                    onPressed:
+                                        () {},
+                                    child: Text(
+                                      countryDetails[0]
+                                          .borders[index],
+                                      style: TextStyle(
+                                        fontSize:
+                                            14,
+                                        color:
+                                            textColor,
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
                             ),
                           ),
-
-                          SizedBox(height: 10),
-                          Divider(),
-                        ],
-                      ),
+                        );
+                      },
                     ),
-                  ),
-                  SliverToBoxAdapter(
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.only(
-                            right: 20,
-                            left: 20,
-                            bottom: 5,
-                          ),
-                      child: Column(
-                        mainAxisAlignment:
-                            MainAxisAlignment
-                                .center,
-                        crossAxisAlignment:
-                            CrossAxisAlignment
-                                .start,
-                        children: [
-                          Text(
-                            'Borders: ${widget.country.borders.join(', ')}',
-                            style: TextStyle(
-                              fontSize: 24,
-
-                              color: textColor,
-                            ),
-                          ),
-
-                          SizedBox(height: 10),
-                          Divider(),
-                        ],
-                      ),
-                    ),
+                  ],
+                  SliverPadding(
+                    padding:
+                        EdgeInsetsGeometry.only(
+                          bottom: 100,
+                        ),
                   ),
                 ],
               ],
